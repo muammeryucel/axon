d5882dfc7f55e23ac19ac2ddf8480a293404bfc1
myucel <myucel@harezmi.com.tr>
2020-02-17 13:40:40 +0300

    initial commit

---
+++ bank-account/src/main/java/com/example/account/BankAccountApplication.java
@@ -0,0 +1,15 @@
+package com.example.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountApplication.class, args);
+	}
+
+	
+	
+}
